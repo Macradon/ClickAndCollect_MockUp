87ec8daa666faa70c37a1ccbab367c6defe75f55
Macradon <nguyen.tri0c@gmail.com>
2024-12-07 22:40:00 +0100

    Created datamodels
    Datamodels based on the systemdesign document has been implemented with the correct relationships

--- ClickAndCollectMockUp/base/models.py
+++ ClickAndCollectMockUp/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_status = models.CharField(max_length=200, default="Created")
+
+class OrderLine(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item_number = models.CharField(max_length=200)
+    amount = models.IntegerField()
+    order_line_status = models.CharField(max_length=200, default="Not-ready")
+
+class OrderLog(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField()
+    incident = models.CharField(max_length=200, default='redacted')
